92f33ec6191f53e2a207ea751e446b44b5830333
22p21s0045-อัครวิทย์ <71715345+22p21s0045@users.noreply.github.com>
2022-02-22 18:22:05 +0700

    [ADD]-Set-state

--- components/Fill.tsx
+++ components/Fill.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { TextInput, Box, Grid, Button, Calendar } from "grommet";
 import FormControl from "@mui/material/FormControl";
-import Select,{ SelectChangeEvent } from "@mui/material/Select";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import InputLabel from "@mui/material/InputLabel";
 import { useState } from "react";
 function Fill() {
   const [message, setmessage] = useState("");
-  const[tag, settag] = useState("");
+  const [tag, settag] = useState("");
   const handleTag = (event: SelectChangeEvent) => {
     settag(event.target.value as string);
   };
-  const [date, setdate] = useState();
+  const [date, setdate] = useState<string | string[]>();
   return (
     <div>
       <Grid align="center">
@@ -22,8 +22,14 @@ function Fill() {
           className="Fill"
           align="center"
         >
-          <TextInput placeholder="Message" value={message} onChange ={event => setmessage(event.target.value)} size="small" />
-
+          <div style={{ paddingTop: 50 }}>
+            <TextInput
+              placeholder="Message"
+              value={message}
+              onChange={(event) => setmessage(event.target.value)}
+              size="large"
+            />
+          </div>
           <FormControl style={{ paddingTop: 50 }}>
             <InputLabel style={{ paddingTop: 50 }}>Tag</InputLabel>
             <Select
@@ -34,21 +40,23 @@ function Fill() {
               color="primary"
               onChange={handleTag}
             >
-              <MenuItem value ={"Hello"}>Hello</MenuItem>
-              <MenuItem value ={"World"}>World</MenuItem>
-              <MenuItem value ={""}>None</MenuItem>
+              <MenuItem value={"Hello"}>Hello</MenuItem>
+              <MenuItem value={"World"}>World</MenuItem>
+              <MenuItem value={""}>None</MenuItem>
             </Select>
           </FormControl>
-          <Calendar size="small" style={{ paddingTop: 50 }} 
-          date={(new Date()).toISOString()}
-          onSelect={(date) => setdate(date)}
+          <Calendar
+            size="small"
+            style={{ paddingTop: 50 }}
+            date={new Date().toISOString().substring(0, 10)}
+            onSelect={(date) => setdate(date)}
           />
           <h1>{message}</h1>
           <h3>{tag}</h3>
           <h3>{date}</h3>
           <div style={{ paddingTop: 50 }}>
-            
             <Button label="Submit" />
+            <Button label="Clear" />
           </div>
         </Box>
       </Grid>
