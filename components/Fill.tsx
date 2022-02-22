c8fd4ee6007e9d4adca75f35c833232abda8e77a
22p21s0045-อัครวิทย์ <71715345+22p21s0045@users.noreply.github.com>
2022-02-22 13:28:22 +0700

    [ADD]-submit-button

--- components/Fill.tsx
+++ components/Fill.tsx
@@ -1,16 +1,41 @@
-import React from 'react'
-import{TextInput,Box,Grid} from 'grommet';
+import React from "react";
+import { TextInput, Box, Grid,Button } from "grommet";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import InputLabel from "@mui/material/InputLabel";
 function Fill() {
   return (
-    <div >
-      <Grid align ="center">
-      <Box direction="column" animation="fadeIn" gridArea='main' className="Fill" align="center">
-        <TextInput placeholder="Message" size="small" />\
-        <h1>hello opacity</h1>
+    <div>
+      <Grid align="center">
+        <Box
+          direction="column"
+          animation="fadeIn"
+          gridArea="main"
+          className="Fill"
+          align="center"
+        >
+          <TextInput placeholder="Message" size="small"  />
+          
+          <FormControl style={{paddingTop:50}}>
+            <InputLabel style={{paddingTop:50}}>Tag</InputLabel>
+            <Select
+              label="Tag"
+              size="small"
+              placeholder="Tag"
+              style={{ width: 100 ,height: 50 }}
+              color="primary"
+            >
+              <MenuItem>Hello</MenuItem>
+            </Select>
+          </FormControl>
+          <div style={{paddingTop:50}}>
+          <Button label="Submit" />
+          </div>
         </Box>
-        </Grid>
+      </Grid>
     </div>
-  )
+  );
 }
 
-export default Fill
+export default Fill;
