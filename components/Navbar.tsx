fb0f820836c947f0ff31aa78a0929ebe6b9592cd
lommel <monkeytitle@hotmail.com>
2022-02-23 13:56:35 +0700

    [ADD]-.env

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,24 @@
 import React from "react";
 import { Box, Anchor, Avatar, Header, Nav, Button } from "grommet";
+import { useSession, signIn, signOut } from "next-auth/react"
 function Navbar() {
+  const { data: session } = useSession()
+  if (session){
+    <div>
+      <Header background="#FFBBBB">
+          <Box style={{paddingLeft:10}}>
+        <Avatar align="start">
+          <Avatar background="black" size="medium" />
+        </Avatar>
+        </Box>
+
+        <Box animation="pulse" alignContent="end" pad="medium">
+          <Button onClick ={()=>signOut}label="LOGOUT" />
+        </Box>
+      </Header>
+    </div>
+
+  }
   return (
     <div>
       <Header background="#FFBBBB">
@@ -11,7 +29,7 @@ function Navbar() {
         </Box>
 
         <Box animation="pulse" alignContent="end" pad="medium">
-          <Button label="LOGIN" />
+          <Button onClick ={()=>signIn} label="LOGIN" />
         </Box>
       </Header>
     </div>
